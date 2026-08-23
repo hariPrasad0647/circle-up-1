854cf740caaacb898882a05df7f397160fa40694
Hari Prasad <hariprasad0647@gmail.com>
2026-08-23 12:50:07 +0530

    chore: increase express JSON and URL-encoded request body size limits to 100mb

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ const app = express();
 app.use(helmet());
 app.use(cors({ origin: true, credentials: true }));
 app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
